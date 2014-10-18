c53913a5cf4369675ed2185b393e92cc6d7e10c8
Luiz <llope039@ucr.edu>
2014-10-18 09:58:20 -0700

    Completed Parsing with semicolons

--- src/main.cc
+++ src/main.cc
@@ -4,9 +4,19 @@
 #include <sys/types.h>
 #include <string.h>
 #include <vector>
+#include <string>
+
+
 using namespace std;
 
+bool execute( vector<char *> command)
+{
+	bool good_exc = true;
+	for(int i = 0; i < command.size(); ++i)cout << command.at(i);		
+	cout << "EXECUTING CMD...." << endl;
 
+	return good_exc;	
+}
 
 int main()
 {
@@ -20,7 +30,10 @@ int main()
 		cout << name <<"@"<< host <<"$";
 		getline(cin, input);
 		if(input == "exit")
-		no_exit = false;
+		{
+			no_exit = false;
+			break;
+		}
 		
 		char *token = strtok(const_cast<char*>(input.c_str()), " ");
 		vector<char *> input_list;
@@ -29,12 +42,26 @@ int main()
 			input_list.push_back(token);
 			token = strtok (NULL, " ");
 		}
-		cout << "##### PRINTING INPUT ARG#####" << endl;
-		//remember to remove for loop 
+		
+		bool proper_exc = true;
+		vector<char *>cmd_run; 
+		int count = 0; 
 		for(int i = 0; i < input_list.size();++i)
-		{
-			cout << input_list.at(i) << endl;
+		{	
+			
+			string temp = input_list.at(i);
+			cmd_run.push_back(input_list.at(i));
+			++count;
+			if(temp.find(';') != string::npos)
+			{
+				temp = temp.substr(0, temp.find(';'));
+				cmd_run.at(count-1) = const_cast<char *>(temp.c_str());
+				proper_exc = execute(cmd_run);
+				cmd_run.clear();
+				count = 0;
+			}
 		}
+		proper_exc = execute(cmd_run);
 	}
 	return 0;
 }
